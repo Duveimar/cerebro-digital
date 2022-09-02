a8bec03ed80f0d1c0bed35abf8d3aa97eefcc248
Duveimar <arevalomeloduveimar@gmail.com>
2022-09-02 14:41:51 -0500

    variables con js

---
+++ Avariables.js
@@ -0,0 +1,13 @@
+console.log("Soy Duveimar")
+//soy duveimar
+/**
+ * arevalo
+ * melo
+ */
+// formas de declarar variables 
+var nombre ="Duveimar";
+let apellido ="Arevalo";
+const dni ="65464";
+
+
+console.log(nombre, apellido);
